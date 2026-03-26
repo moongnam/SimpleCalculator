34798a88d735df180280395b11f12f9321e0ad96
kms <kms060111@naver.com>
2026-03-26 11:26:46 +0900

    과제1 코딩과 테스트 완료 2개의 TextBox를 배치하여 txtNum1, txtResult라는 이름을 붙였습니다. 총 20개의 Button을 사용하여 계산기의 기본적인 기능을 구현하였고. 사칙연산 중에서 덧셈을 우선적으로 구현하였습니다. 상단의 TextBox에 입력 내용이 그대로 모두 표시되게끔 하였습니다.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,357 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            txtTitle = new Label();
+            txtNum1 = new TextBox();
+            txtResult = new TextBox();
+            buttonCE = new Button();
+            buttonC = new Button();
+            buttonDel = new Button();
+            buttonD = new Button();
+            button_7 = new Button();
+            buttonT = new Button();
+            buttonM = new Button();
+            buttonP = new Button();
+            buttonR = new Button();
+            button_8 = new Button();
+            button_9 = new Button();
+            button_4 = new Button();
+            button_5 = new Button();
+            button_6 = new Button();
+            button_1 = new Button();
+            button_2 = new Button();
+            button_3 = new Button();
+            button_Pm = new Button();
+            button_0 = new Button();
+            button_dot = new Button();
+            SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            txtTitle.AutoSize = true;
+            txtTitle.BackColor = Color.AliceBlue;
+            txtTitle.Font = new Font("한컴 말랑말랑 Bold", 24F, FontStyle.Bold, GraphicsUnit.Point, 129, true);
+            txtTitle.ForeColor = Color.LightSkyBlue;
+            txtTitle.Location = new Point(105, 32);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(353, 52);
+            txtTitle.TabIndex = 0;
+            txtTitle.Text = "Simple Calculator";
+            txtTitle.Click += nameLabel1_Click;
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(82, 98);
+            txtNum1.Multiline = true;
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(398, 36);
+            txtNum1.TabIndex = 1;
+            txtNum1.TextChanged += txtNum1_TextChanged;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(82, 149);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(398, 36);
+            txtResult.TabIndex = 2;
+            txtResult.TextChanged += textBox1_TextChanged_1;
+            // 
+            // buttonCE
+            // 
+            buttonCE.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonCE.Location = new Point(82, 204);
+            buttonCE.Name = "buttonCE";
+            buttonCE.Size = new Size(94, 52);
+            buttonCE.TabIndex = 3;
+            buttonCE.Text = "CE";
+            buttonCE.UseVisualStyleBackColor = true;
+            buttonCE.Click += buttonCE_Click;
+            // 
+            // buttonC
+            // 
+            buttonC.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonC.Location = new Point(186, 204);
+            buttonC.Name = "buttonC";
+            buttonC.Size = new Size(94, 52);
+            buttonC.TabIndex = 4;
+            buttonC.Text = "C";
+            buttonC.UseVisualStyleBackColor = true;
+            // 
+            // buttonDel
+            // 
+            buttonDel.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonDel.Location = new Point(286, 204);
+            buttonDel.Name = "buttonDel";
+            buttonDel.Size = new Size(94, 52);
+            buttonDel.TabIndex = 4;
+            buttonDel.Text = "Del";
+            buttonDel.UseVisualStyleBackColor = true;
+            // 
+            // buttonD
+            // 
+            buttonD.Font = new Font("휴먼둥근헤드라인", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonD.ForeColor = Color.Brown;
+            buttonD.Location = new Point(386, 204);
+            buttonD.Name = "buttonD";
+            buttonD.Size = new Size(94, 52);
+            buttonD.TabIndex = 5;
+            buttonD.Text = "÷";
+            buttonD.UseVisualStyleBackColor = true;
+            // 
+            // button_7
+            // 
+            button_7.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_7.ForeColor = Color.SteelBlue;
+            button_7.Location = new Point(82, 262);
+            button_7.Name = "button_7";
+            button_7.Size = new Size(94, 52);
+            button_7.TabIndex = 6;
+            button_7.Text = "7";
+            button_7.UseVisualStyleBackColor = true;
+            // 
+            // buttonT
+            // 
+            buttonT.Font = new Font("함초롬돋움", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonT.ForeColor = Color.Brown;
+            buttonT.Location = new Point(386, 262);
+            buttonT.Name = "buttonT";
+            buttonT.Size = new Size(94, 52);
+            buttonT.TabIndex = 7;
+            buttonT.Text = "x";
+            buttonT.UseVisualStyleBackColor = true;
+            // 
+            // buttonM
+            // 
+            buttonM.Font = new Font("함초롬돋움", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonM.ForeColor = Color.Brown;
+            buttonM.Location = new Point(386, 320);
+            buttonM.Name = "buttonM";
+            buttonM.Size = new Size(94, 52);
+            buttonM.TabIndex = 8;
+            buttonM.Text = "-";
+            buttonM.UseVisualStyleBackColor = true;
+            // 
+            // buttonP
+            // 
+            buttonP.Font = new Font("함초롬돋움", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonP.ForeColor = Color.Brown;
+            buttonP.Location = new Point(386, 378);
+            buttonP.Name = "buttonP";
+            buttonP.Size = new Size(94, 52);
+            buttonP.TabIndex = 9;
+            buttonP.Text = "+";
+            buttonP.UseVisualStyleBackColor = true;
+            // 
+            // buttonR
+            // 
+            buttonR.Font = new Font("함초롬돋움", 18F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            buttonR.ForeColor = Color.Brown;
+            buttonR.Location = new Point(386, 436);
+            buttonR.Name = "buttonR";
+            buttonR.Size = new Size(94, 52);
+            buttonR.TabIndex = 10;
+            buttonR.Text = "=";
+            buttonR.UseVisualStyleBackColor = true;
+            // 
+            // button_8
+            // 
+            button_8.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_8.ForeColor = Color.SteelBlue;
+            button_8.Location = new Point(186, 262);
+            button_8.Name = "button_8";
+            button_8.Size = new Size(94, 52);
+            button_8.TabIndex = 11;
+            button_8.Text = "8";
+            button_8.UseVisualStyleBackColor = true;
+            // 
+            // button_9
+            // 
+            button_9.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_9.ForeColor = Color.SteelBlue;
+            button_9.Location = new Point(286, 262);
+            button_9.Name = "button_9";
+            button_9.Size = new Size(94, 52);
+            button_9.TabIndex = 12;
+            button_9.Text = "9";
+            button_9.UseVisualStyleBackColor = true;
+            // 
+            // button_4
+            // 
+            button_4.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_4.ForeColor = Color.SteelBlue;
+            button_4.Location = new Point(82, 320);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(94, 52);
+            button_4.TabIndex = 13;
+            button_4.Text = "4";
+            button_4.UseVisualStyleBackColor = true;
+            // 
+            // button_5
+            // 
+            button_5.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_5.ForeColor = Color.SteelBlue;
+            button_5.Location = new Point(186, 320);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(94, 52);
+            button_5.TabIndex = 14;
+            button_5.Text = "5";
+            button_5.UseVisualStyleBackColor = true;
+            // 
+            // button_6
+            // 
+            button_6.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_6.ForeColor = Color.SteelBlue;
+            button_6.Location = new Point(286, 320);
+            button_6.Name = "button_6";
+            button_6.Size = new Size(94, 52);
+            button_6.TabIndex = 15;
+            button_6.Text = "6";
+            button_6.UseVisualStyleBackColor = true;
+            // 
+            // button_1
+            // 
+            button_1.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_1.ForeColor = Color.SteelBlue;
+            button_1.Location = new Point(82, 378);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(94, 52);
+            button_1.TabIndex = 16;
+            button_1.Text = "1";
+            button_1.UseVisualStyleBackColor = true;
+            // 
+            // button_2
+            // 
+            button_2.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_2.ForeColor = Color.SteelBlue;
+            button_2.Location = new Point(186, 378);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(94, 52);
+            button_2.TabIndex = 17;
+            button_2.Text = "2";
+            button_2.UseVisualStyleBackColor = true;
+            // 
+            // button_3
+            // 
+            button_3.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_3.ForeColor = Color.SteelBlue;
+            button_3.Location = new Point(286, 378);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(94, 52);
+            button_3.TabIndex = 18;
+            button_3.Text = "3";
+            button_3.UseVisualStyleBackColor = true;
+            // 
+            // button_Pm
+            // 
+            button_Pm.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_Pm.Location = new Point(82, 436);
+            button_Pm.Name = "button_Pm";
+            button_Pm.Size = new Size(94, 52);
+            button_Pm.TabIndex = 19;
+            button_Pm.Text = "+ / -";
+            button_Pm.UseVisualStyleBackColor = true;
+            // 
+            // button_0
+            // 
+            button_0.Font = new Font("한컴 말랑말랑 Regular", 13.7999992F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            button_0.ForeColor = Color.SteelBlue;
+            button_0.Location = new Point(186, 436);
+            button_0.Name = "button_0";
+            button_0.Size = new Size(94, 52);
+            button_0.TabIndex = 20;
+            button_0.Text = "0";
+            button_0.UseVisualStyleBackColor = true;
+            // 
+            // button_dot
+            // 
+            button_dot.Font = new Font("한컴 말랑말랑 Bold", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button_dot.Location = new Point(286, 436);
+            button_dot.Name = "button_dot";
+            button_dot.Size = new Size(94, 52);
+            button_dot.TabIndex = 21;
+            button_dot.Text = ".";
+            button_dot.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.AliceBlue;
+            ClientSize = new Size(569, 535);
+            Controls.Add(button_dot);
+            Controls.Add(button_0);
+            Controls.Add(button_Pm);
+            Controls.Add(button_3);
+            Controls.Add(button_2);
+            Controls.Add(button_1);
+            Controls.Add(button_6);
+            Controls.Add(button_5);
+            Controls.Add(button_4);
+            Controls.Add(button_9);
+            Controls.Add(button_8);
+            Controls.Add(buttonR);
+            Controls.Add(buttonP);
+            Controls.Add(buttonM);
+            Controls.Add(buttonT);
+            Controls.Add(button_7);
+            Controls.Add(buttonD);
+            Controls.Add(buttonDel);
+            Controls.Add(buttonC);
+            Controls.Add(buttonCE);
+            Controls.Add(txtResult);
+            Controls.Add(txtNum1);
+            Controls.Add(txtTitle);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            // 필요 시 구현. 현재는 빈 구현으로 예외 방지.
+        }
+
+        private void txtNum1_TextChanged(object sender, EventArgs e)
+        {
+            // 필요 시 구현. 현재는 빈 구현으로 예외 방지.
+        }
+
+        private void nameLabel1_Click(object sender, EventArgs e)
+        {
+            // 필요 시 구현. 현재는 빈 구현으로 예외 방지.
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            // 필요 시 구현. 현재는 빈 구현으로 예외 방지.
         }
 
         #endregion
+
+        private Label txtTitle;
+        private TextBox txtNum1;
+        private TextBox txtResult;
+        private Button buttonCE;
+        private Button buttonC;
+        private Button buttonDel;
+        private Button buttonD;
+        private Button button_7;
+        private Button buttonT;
+        private Button buttonM;
+        private Button buttonP;
+        private Button buttonR;
+        private Button button_8;
+        private Button button_9;
+        private Button button_4;
+        private Button button_5;
+        private Button button_6;
+        private Button button_1;
+        private Button button_2;
+        private Button button_3;
+        private Button button_Pm;
+        private Button button_0;
+        private Button button_dot;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,87 @@ namespace SimpleCalculator
 {
     public partial class Form1 : Form
     {
+        private double _firstNum = 0;      // 첫 번째 숫자 저장
+        private string _operator = "";     // 연산자 저장 (+, -, *, /)
+        private bool _isNewNum = true;     // 새로 숫자를 입력할 차례인지 확인
+
         public Form1()
         {
             InitializeComponent();
+            // 숫자 버튼 이벤트 연결 (0~9, 점)
+            button_0.Click += NumberButton_Click;
+            button_1.Click += NumberButton_Click;
+            button_2.Click += NumberButton_Click;
+            button_3.Click += NumberButton_Click;
+            button_4.Click += NumberButton_Click;
+            button_5.Click += NumberButton_Click;
+            button_6.Click += NumberButton_Click;
+            button_7.Click += NumberButton_Click;
+            button_8.Click += NumberButton_Click;
+            button_9.Click += NumberButton_Click;
+
+            // 연산자 및 기능 버튼
+            buttonP.Click += OperatorButton_Click; // +
+            buttonR.Click += EqualButton_Click;    // =
+            buttonCE.Click += (s, e) => { txtResult.Text = "0"; _isNewNum = true; };
+            buttonC.Click += ClearAll;
+        }
+
+        // 1. 숫자 버튼 클릭 시
+        private void NumberButton_Click(object? sender, EventArgs? e)
+        {
+            if (sender is not Button btn) return;
+
+            // 새로운 숫자를 입력해야 하거나 현재 '0'이면 지우고 새로 씀
+            if (_isNewNum || txtResult.Text == "0")
+            {
+                txtResult.Text = btn.Text;
+                _isNewNum = false;
+            }
+            else
+            {
+                txtResult.Text += btn.Text;
+            }
+        }
+
+        // 2. 연산자(+) 버튼 클릭 시
+        private void OperatorButton_Click(object? sender, EventArgs? e)
+        {
+            if (sender is not Button btn) return;
+
+            _firstNum = double.Parse(txtResult.Text); // 현재 입력된 값을 첫 번째 숫자로 저장
+            _operator = btn.Text;                     // "+" 저장
+
+            // 위 칸(txtNum1)에 "5 + " 처럼 표시
+            txtNum1.Text = _firstNum + " " + _operator + " ";
+
+            _isNewNum = true; // 이제 두 번째 숫자를 받을 준비
+        }
+
+        // 3. 결과(=) 버튼 클릭 시
+        private void EqualButton_Click(object? sender, EventArgs? e)
+        {
+            if (string.IsNullOrEmpty(_operator)) return;
+
+            double secondNum = double.Parse(txtResult.Text);
+            double result = 0;
+
+            if (_operator == "+") result = _firstNum + secondNum;
+
+            txtNum1.Text = $"{_firstNum} {_operator} {secondNum} = {result}";
+            txtResult.Text = result.ToString();
+
+            _isNewNum = true;
+            _operator = "";
+        }
+
+        private void ClearAll(object? sender, EventArgs? e)
+        {
+            _firstNum = 0;
+            _operator = "";
+            _isNewNum = true;
+            txtNum1.Text = "";
+            txtResult.Text = "0";
         }
     }
 }
